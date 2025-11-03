77c6da053c8400b8180f9bf69b56cb81c1ce5bda
Prasanth R <prasanth.r@skillrank.io>
2025-11-03 16:41:54 +0530

    Checkpoint 1 - cg2cff98a6

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /tmp/kavia/workspace/code-generation/classic-ping-pong-game-194671-194680/ping_pong_frontend
+flutter analyze
+
---
+++ ping_pong_frontend/android/app/src/main/kotlin/com/example/ping_pong_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ping_pong_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
